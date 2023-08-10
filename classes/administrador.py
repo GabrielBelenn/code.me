847e5dc7eecdd97fe7544181dab69be964e4f43a
Gabriel Belem <139159012+GabrielBelenn@users.noreply.github.com>
2023-08-09 22:15:01 -0300

    Classes em Python

---
+++ classes/administrador.py
@@ -0,0 +1,10 @@
+from usuario import Usuario
+
+class Administrador(Usuario):
+    def imprime_usuario(self):
+        print(f"{self.nome} ({self.email}) - Administrador") 
+
+     
+    
+    
+        
---
+++ classes/main.py
@@ -0,0 +1,11 @@
+from usuario import Usuario
+from administrador import Administrador
+
+u = Usuario("Gabriel", "gabriel@exemplo.com")
+a = Administrador("Admin", "admin@exemplo.com")
+
+u.imprime_usuario()
+a.imprime_usuario()
+
+print(f"{Usuario.quantidade} instâncias.")
+
